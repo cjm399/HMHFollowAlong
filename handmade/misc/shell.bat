2e2fe8d5cde65183c85da4f909c986c0035770f4
Chris McCole <chrismccole1@gmail.com>
2021-01-06 18:32:58 -0500

    Update shell.bat

--- handmade/misc/shell.bat
+++ handmade/misc/shell.bat
@@ -5,6 +5,6 @@ REM  To run this at startup, use this as your shortcut target:
 REM  %windir%\system32\cmd.exe /k w:\handmade\misc\shell.bat
 REM
 
-subst H: D:\HMH\HMHFollowAlong
+subst H: D:\HMH\
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set path=h:\handmade\misc;%path%
